df27bbaf839c27550b6e113323af7930efcdeee3
Andrew <andrayvirus@gmail.com>
2019-01-10 17:17:12 +0100

    Small DBM Test fixes and added one more test case for Parser

--- test/parser/ParserTest.java
+++ test/parser/ParserTest.java
@@ -1,9 +1,6 @@
 package parser;
 
-import models.Channel;
-import models.Component;
-import models.Location;
-import models.Transition;
+import models.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -14,7 +11,8 @@ import java.util.List;
 
 public class ParserTest {
     private static List<Component> machines;
-    private static Component A, G, Q, Imp;
+    private static List<Component> machines2;
+    private static Component A, G, Q, Imp, Ref1;
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
@@ -64,6 +62,61 @@ public class ParserTest {
         G = new Component("G", new ArrayList<>(Arrays.asList(l3)), new ArrayList<>(Arrays.asList(t4, t5, t6)), new ArrayList<>());
         Q = new Component("Q", new ArrayList<>(Arrays.asList(l5, u0)), new ArrayList<>(Arrays.asList(t7, t8, t9)), new ArrayList<>());
         Imp = new Component("Imp", new ArrayList<>(Arrays.asList(l0, l1)), new ArrayList<>(Arrays.asList(t10, t11, t12, t13, t14, t15, t16)), new ArrayList<>());
+
+
+        // Adding BigRefinement example automata
+        base = "./samples/BigRefinement/";
+        components = new ArrayList<>(Arrays.asList("GlobalDeclarations.json",
+                "Components/Ref1.json"));
+        machines2 = Parser.parse(base, components);
+
+        Clock x = new Clock("x");
+        Clock y = new Clock("y");
+
+        models.Guard g_l12_l17 = new Guard(x, 15,false, true);
+        models.Guard g_l12_l14 = new Guard(x, 20,false, true);
+        models.Guard g_l12_l13 = new Guard(x, 5,false, true);
+        models.Guard g_l12_l15 = new Guard(x, 8,false, true);
+        models.Guard g_l12_l16 = new Guard(x, 55,false, true);
+
+        models.Guard g_l15_l18 = new Guard(x, 15,true, true);
+
+        Update u1 = new Update(x, 0);
+
+        Location l12 = new Location("L12", null, true, false, false, false);
+        Location l13 = new Location("L13", null, false, false, false, false);
+        Location l14 = new Location("L14", null, false, false, false, false);
+        Location l15 = new Location("L15", null, false, false, false, false);
+        Location l16 = new Location("L16", null, false, false, false, false);
+        Location l17 = new Location("L17", null, false, false, false, false);
+        Location l18 = new Location("L18", null, false, false, false, false);
+
+        Channel i1 = new Channel("i1"); Channel i2 = new Channel("i2");
+        Channel i3 = new Channel("i3"); Channel i4 = new Channel("i4");
+        Channel i5 = new Channel("i5"); Channel i6 = new Channel("i6");
+        Channel o1 = new Channel("o1"); Channel o2 = new Channel("o2");
+        Channel o3 = new Channel("o3"); Channel o4 = new Channel("o4");
+        Channel o5 = new Channel("o5"); Channel o6 = new Channel("o6");
+        Channel o7 = new Channel("o7"); Channel o8 = new Channel("o8");
+        Channel o9 = new Channel("o9"); Channel o10 = new Channel("o10");
+
+
+        t1 = new Transition(l12, l14, i2, true, new ArrayList<>(Arrays.asList(g_l12_l14)), new ArrayList<>());
+        t2 = new Transition(l12, l17, i3, true, new ArrayList<>(Arrays.asList(g_l12_l17)), new ArrayList<>());
+        t3 = new Transition(l12, l15, i4, true, new ArrayList<>(Arrays.asList(g_l12_l15)), new ArrayList<>(Arrays.asList(u1)));
+        t4 = new Transition(l12, l16, i5, true, new ArrayList<>(Arrays.asList(g_l12_l16)), new ArrayList<>());
+        t5 = new Transition(l17, l18, o8, false, new ArrayList<>(), new ArrayList<>(Arrays.asList(u1)));
+        t6 = new Transition(l16, l18, o8, false, new ArrayList<>(), new ArrayList<>());
+        t7 = new Transition(l15, l18, o8, false, new ArrayList<>(Arrays.asList(g_l15_l18)), new ArrayList<>());
+        t8 = new Transition(l14, l18, o8, false, new ArrayList<>(), new ArrayList<>());
+        t9 = new Transition(l13, l18, o8, false, new ArrayList<>(), new ArrayList<>());
+        t10 = new Transition(l17, l17, o3, false, new ArrayList<>(), new ArrayList<>());
+        t11 = new Transition(l17, l17, o5, false, new ArrayList<>(), new ArrayList<>());
+        t12 = new Transition(l17, l14, i6, true, new ArrayList<>(), new ArrayList<>());
+        t13 = new Transition(l12, l13, i1, true, new ArrayList<>(Arrays.asList(g_l12_l13)), new ArrayList<>());
+
+        Ref1 = new Component("Ref1", new ArrayList<>(Arrays.asList(l12, l13, l14, l15, l16, l17, l18)),
+                new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13)), new ArrayList<>(Arrays.asList(x, y)));
     }
 
     @Test
@@ -84,5 +137,10 @@ public class ParserTest {
     @Test
     public void testImp() {
         assert Imp.equals(machines.get(3));
+    }
+
+    @Test
+    public void testRef1() {
+        assert Ref1.equals(machines2.get(0));
     }
 }
