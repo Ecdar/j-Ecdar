3fb42dc1e13e00a1dc175a946c6bbab7679c6709
napalys.klicius@gmail.com <napalys.klicius@gmail.com>
2019-01-12 16:45:33 +0100

    Some imporvements to connection layer as well as their tests

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,48 +2,63 @@ package main;
 
 import logic.Controller;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static final String VERSION = "1.0";
+    private static Controller ctrl = new Controller();
+
     public static void main(String[] args) {
-        //mapDBM();
-
         Scanner console = new Scanner(System.in);
-        System.out.println("Server has started");
-        System.out.println("Waiting for input...");
-        Controller ctrl = new Controller();
-        while(console.hasNextLine()) {
-            String reader = console.nextLine();
-            String indicator = reader;
-            if(reader.contains(" ")){indicator = reader.substring(0, reader.indexOf(' '));}
-
-            switch(indicator.toLowerCase())
-            {
-                case "-version":
-                    System.out.println("Version "+VERSION);
-                    break;
-                case "-rq":
-                    if(reader.contains(" ")){try{System.out.println(ctrl.parseFiles(reader.substring(reader.indexOf(' ')+1,reader.length())).get(0));}
-                    catch(Exception e){e.printStackTrace();}}
-                    else System.out.println("Try using correct syntax -rq path query query query");
-                    break;
-
-                case "-vq":
-                    try {
-                        System.out.println(ctrl.isQueryValid(console.nextLine()));
-                    }
-                    catch(Exception e){e.printStackTrace();}
-                    break;
-                default:
-                    System.out.println("Server confirms having received: " + reader + " Invalid command");
-            }
+        while (console.hasNextLine()) {
+            System.out.println(chooseCommand(console.nextLine()));
         }
     }
 
-    private static void mapDBM(){
-        String fileName = "src/" + System.mapLibraryName("DBM");
-        File lib = new File(fileName);
-        System.load(lib.getAbsolutePath());
+    public static String chooseCommand(String query) {
+        String reader = query;
+        String indicator = reader;
+        if (reader.contains(" ")) {
+            indicator = reader.substring(0, reader.indexOf(' '));
+        }
+        switch (indicator.toLowerCase()) {
+            case "-version":
+                return "Version " + VERSION;
+            case "-rq":
+                    try {
+                        List<Boolean> temp = ctrl.parseFiles(reader.substring(reader.indexOf(' ') + 1));
+                        if (temp.size() == 1) return temp.get(0).toString();
+                        else {
+                            String str = "";
+                            for (int i = 0; i < temp.size(); i++) {
+                                str = str + temp.get(i).toString();
+                                if (i + 1 < temp.size()) str += " ";
+                            }
+                            return str;
+                        }
+                    } catch (Exception e) {
+                        return "Error: " + e.getMessage();//e.printStackTrace();
+                    }
+            case "-vq":
+                    try {
+                        Boolean result = ctrl.isQueryValid(reader.substring(reader.indexOf(' ') + 1));
+                        return result.toString();
+                    } catch (Exception e) {
+                        return "Error: " + e.getMessage();//e.printStackTrace();
+                    }
+            case "-help":
+                return "In order to check version type:-version\n"
+                        + "In order to run query type:-rq folderPath query query...\n"
+                        + "In order to check the validity of a query type:-vq query";
+            default:
+                return "Server confirms having received: \"" + reader + "\" try -help";
+        }
     }
 }
+//    private static void mapDBM(){
+//        String fileName = "src/" + System.mapLibraryName("DBM");
+//        File lib = new File(fileName);
+//        System.load(lib.getAbsolutePath());
+
