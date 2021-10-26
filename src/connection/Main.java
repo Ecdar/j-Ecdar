955217d27ce10e9c11e866cd093010ae2b2adc57
Nicklas Hjortshøj Hugøy <Nicklas.hug@gmail.com>
2021-10-26 16:30:47 +0200

    remove unused options

--- src/connection/Main.java
+++ src/connection/Main.java
@@ -33,63 +33,10 @@ class Main {
             .longOpt("help")
             .build();
 
-    static Option comps = Option.builder()
-            .longOpt("components")
-            .argName("comps")
-            .hasArgs()
-            .desc("Components to load")
-            .required(false)
-            .build();
-
-    static Option refinement = Option.builder()
-            .longOpt("refinement")
-            .desc("Query for refinement check")
-            .required(false)
-            .build();
-
-    static Option get = Option.builder()
-            .longOpt("get-new-component")
-            .desc("Produce an automaton for the query")
-            .required(false)
-            .build();
-
-    static Option quo = Option.builder()
-            .longOpt("quotient")
-            .desc("produce the quotient")
-            .required(false)
-            .build();
-
-    static Option bsim = Option.builder()
-            .longOpt("bisim-min")
-            .argName("bsim")
-            .hasArg(false)
-            .desc("minimize the automaton via bisimilarity check")
-            .required(false)
-            .build();
-
-    static Option prn = Option.builder()
-            .longOpt("prune")
-            .argName("prn")
-            .hasArg(false)
-            .desc("prune inconsistent states (name needs to be inc) from the automata")
-            .required(false)
-            .build();
-
-
-    Option spec = new Option("specification", "check if this is a specification");
-    Option imp = new Option("implementation", "check if this is an implementation");
-    Option lcon = new Option("local-consistency", "check for local consistency");
-    Option gcon = new Option("global-consistency", "check for global consistency");
 
     public static void main(String[] args) {
 
         options.addOption(inputFolder);
-        options.addOption(refinement);
-        options.addOption(comps);
-        options.addOption(quo);
-        options.addOption(get);
-        options.addOption(prn);
-        options.addOption(bsim);
         options.addOption(help);
 
         CommandLineParser parser = new DefaultParser();
@@ -112,11 +59,6 @@ class Main {
             StringBuilder argStrBuilder = new StringBuilder();
             argsList.forEach(argStrBuilder::append);
             String queryString = argStrBuilder.toString();
-
-            boolean prune = cmd.hasOption("prune");
-            boolean bisim = cmd.hasOption("bsim-min");
-            boolean refinment = cmd.hasOption("refinement");
-            boolean getNewComponent = cmd.hasOption("get-new-component");
 
             try {
                 System.out.println("-json " + inputFolderPath + " " + queryString);
