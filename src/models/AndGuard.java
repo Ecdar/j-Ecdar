c0c27993143e7a9689c47405426aadd76aaa7196
Andreas <akbr18@student.aau.dk>
2022-07-07 09:40:52 +0200

    format (AndGuard): Formatted AndGuard using IntelliJ

--- src/models/AndGuard.java
+++ src/models/AndGuard.java
@@ -2,15 +2,14 @@ package models;
 
 import java.util.*;
 
-public class AndGuard extends Guard{
+public class AndGuard extends Guard {
 
     private List<Guard> guards;
 
-    public AndGuard(List<Guard> guards)
-    {
-        this.guards=guards;
+    public AndGuard(List<Guard> guards) {
+        this.guards = guards;
         List<Integer> indices = new ArrayList<>();
-        for (int i=0; i<this.guards.size(); i++)
+        for (int i = 0; i < this.guards.size(); i++)
             if (this.guards.get(i) instanceof TrueGuard)
                 indices.add(i);
         Collections.reverse(indices);
@@ -19,15 +18,15 @@ public class AndGuard extends Guard{
         if (this.guards.isEmpty())
             this.guards.add(new TrueGuard());
     }
-    public AndGuard(List<Guard>... guards)
-    {
-        this.guards= new ArrayList<>();
-        for (List<Guard> g: guards)
+
+    public AndGuard(List<Guard>... guards) {
+        this.guards = new ArrayList<>();
+        for (List<Guard> g : guards)
             this.guards.addAll(g);
 
 
         List<Integer> indices = new ArrayList<>();
-        for (int i=0; i<this.guards.size(); i++)
+        for (int i = 0; i < this.guards.size(); i++)
             if (this.guards.get(i) instanceof TrueGuard)
                 indices.add(i);
         Collections.reverse(indices);
@@ -36,14 +35,14 @@ public class AndGuard extends Guard{
         if (this.guards.isEmpty())
             this.guards.add(new TrueGuard());
     }
-    public AndGuard(Guard... guards)
-    {
-        this.guards= new ArrayList<>();
+
+    public AndGuard(Guard... guards) {
+        this.guards = new ArrayList<>();
         for (Guard g : guards)
-           this.guards.add(g);
+            this.guards.add(g);
 
         List<Integer> indices = new ArrayList<>();
-        for (int i=0; i<this.guards.size(); i++)
+        for (int i = 0; i < this.guards.size(); i++)
             if (this.guards.get(i) instanceof TrueGuard)
                 indices.add(i);
         Collections.reverse(indices);
@@ -54,13 +53,11 @@ public class AndGuard extends Guard{
 
     }
 
-    public AndGuard(AndGuard copy, List<Clock> newClocks,List<Clock> oldClocks,   List<BoolVar> newBVs, List<BoolVar> oldBVs)
-    {
-        this.guards=new ArrayList<>();
-        for (Guard g : copy.guards)
-        {
+    public AndGuard(AndGuard copy, List<Clock> newClocks, List<Clock> oldClocks, List<BoolVar> newBVs, List<BoolVar> oldBVs) {
+        this.guards = new ArrayList<>();
+        for (Guard g : copy.guards) {
             if (g instanceof ClockGuard)
-                this.guards.add(new ClockGuard((ClockGuard) g,  newClocks,oldClocks));
+                this.guards.add(new ClockGuard((ClockGuard) g, newClocks, oldClocks));
             if (g instanceof BoolGuard)
                 this.guards.add(new BoolGuard((BoolGuard) g, newBVs, oldBVs));
             if (g instanceof FalseGuard)
@@ -68,12 +65,12 @@ public class AndGuard extends Guard{
             if (g instanceof TrueGuard)
                 this.guards.add(new TrueGuard());
             if (g instanceof AndGuard)
-                this.guards.add(new AndGuard( (AndGuard) g, newClocks, oldClocks, newBVs, oldBVs));
+                this.guards.add(new AndGuard((AndGuard) g, newClocks, oldClocks, newBVs, oldBVs));
             if (g instanceof OrGuard)
-                this.guards.add(new OrGuard( (OrGuard) g, newClocks, oldClocks, newBVs, oldBVs));
+                this.guards.add(new OrGuard((OrGuard) g, newClocks, oldClocks, newBVs, oldBVs));
         }
         List<Integer> indices = new ArrayList<>();
-        for (int i=0; i<guards.size(); i++)
+        for (int i = 0; i < guards.size(); i++)
             if (guards.get(i) instanceof TrueGuard)
                 indices.add(i);
         Collections.reverse(indices);
@@ -88,9 +85,8 @@ public class AndGuard extends Guard{
     @Override
     int getMaxConstant(Clock clock) {
         int max = 0;
-        for (Guard g: guards)
-        {
-            if (g.getMaxConstant(clock)>max)
+        for (Guard g : guards) {
+            if (g.getMaxConstant(clock) > max)
                 max = g.getMaxConstant(clock);
         }
         return max;
@@ -101,11 +97,11 @@ public class AndGuard extends Guard{
         if (!(o instanceof AndGuard))
             return false;
         AndGuard other = (AndGuard) o;
-        if (other.guards.size()!=guards.size())
-            return  false;
-        if (guards.size()==0)
+        if (other.guards.size() != guards.size())
+            return false;
+        if (guards.size() == 0)
             return true;
-        for (int i =0; i< other.guards.size(); i++)
+        for (int i = 0; i < other.guards.size(); i++)
             if (!guards.get(i).equals(other.guards.get(i)))
                 return false;
         return true;
@@ -115,16 +111,14 @@ public class AndGuard extends Guard{
     public String toString() {
 
 
-
-
         String ret = "(";
-        for (Guard g: guards)
+        for (Guard g : guards)
             ret += g.toString() + " && ";
-        if (guards.size()==0)
+        if (guards.size() == 0)
             return "";
-        if (guards.size()==1)
+        if (guards.size() == 1)
             return guards.get(0).toString();
-        return ret.substring(0,ret.length()-4) + ")";
+        return ret.substring(0, ret.length() - 4) + ")";
     }
 
     @Override
