2ca394a9a5fd1e08354bd2b00171bcf3af036427
Brandhoej <akbr18@student.aau.dk>
2022-08-08 12:56:53 +0200

    patch (Move): new-cdd-functions

--- src/models/Move.java
+++ src/models/Move.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Move {
-    private final SymbolicLocation source, target;
+    private SymbolicLocation source, target;
     private final List<Edge> edges;
     private CDD guardCDD;
     private List<Update> updates;
@@ -53,7 +53,7 @@ public class Move {
     }
 
     public CDD getGuardCDD() {
-        return guardCDD;
+        return (guardCDD);
     }
 
     public Guard getGuards(List<Clock> relevantClocks) {
@@ -70,5 +70,9 @@ public class Move {
 
     public void setUpdates(List<Update> updates) {
         this.updates = updates;
+    }
+
+    public void setTarget(SymbolicLocation loc) {
+        target = loc;
     }
 }
