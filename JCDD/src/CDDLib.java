8c3eb5359fc44fa7d401bfe451651a2641afc3c8
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-01-30 12:28:30 +0100

    System library change

--- JCDD/src/CDDLib.java
+++ JCDD/src/CDDLib.java
@@ -20,10 +20,57 @@ public class CDDLib {
             }
         }
 
+        /*
         if (lib != null) {
             System.load(lib.getAbsolutePath());
         } else {
             System.load(searchPath.get(searchPath.size() - 1).getAbsolutePath()); // Default path
+        }
+
+        try {
+            java.io.FileWriter myWriter = new java.io.FileWriter("LOGG", true);
+            myWriter.write(System.lineSeparator());
+            myWriter.write("lib: ");
+            myWriter.write(lib.getAbsolutePath());
+            myWriter.close();
+        }
+        catch (Exception e) {
+            System.out.println("---------------------------");
+            System.out.println(e);
+            System.out.println("---------------------------");
+        }
+        String currentLibraryPath = System.getProperty("java.library.path");
+        if (lib != null) {
+            String newLibraryPath = lib.getAbsolutePath();
+            System.setProperty("java.library.path", currentLibraryPath + File.pathSeparator + newLibraryPath);
+            //System.load(lib.getAbsolutePath());
+        } else {
+            String newLibraryPath = searchPath.get(searchPath.size() - 1).getAbsolutePath();
+            System.setProperty("java.library.path", currentLibraryPath + File.pathSeparator + newLibraryPath);
+            //System.load(searchPath.get(searchPath.size() - 1).getAbsolutePath()); // Default path
+        }
+        try {
+            java.io.FileWriter myWriter = new java.io.FileWriter("LOGG", true);
+            myWriter.write(System.lineSeparator());
+            myWriter.write("\njava.library.path: ");
+            myWriter.write(System.getProperty("java.library.path"));
+            myWriter.close();
+        }
+        catch (Exception e) {
+            System.out.println("---------------------------");
+            System.out.println(e);
+            System.out.println("---------------------------");
+        }
+         */
+        String currentLibraryPath = System.getProperty("java.library.path");
+        if (lib != null) {
+            String newLibraryPath = lib.getAbsolutePath();
+            System.setProperty("java.library.path", currentLibraryPath + File.pathSeparator + newLibraryPath);
+            //System.load(lib.getAbsolutePath());
+        } else {
+            String newLibraryPath = searchPath.get(searchPath.size() - 1).getAbsolutePath();
+            System.setProperty("java.library.path", currentLibraryPath + File.pathSeparator + newLibraryPath);
+            //System.load(searchPath.get(searchPath.size() - 1).getAbsolutePath()); // Default path
         }
     }
 
